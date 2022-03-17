88bb62e61ce62292509f66af07a5ee69357187c0
a <a@trwnh.com>
2022-03-17 12:52:04 -0500

    formatting and docstring cleanup

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import discord
 from discord.ext.commands import Bot, when_mentioned_or
+
 """ Declare intents that the bot will use """
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 intents.reactions = True
+
+""" Load the bot token """
+from dotenv import load_dotenv
+load_dotenv()
+from os import getenv
+TOKEN = getenv("BOT_TOKEN")
+GUILD = getenv("GUILD_ID")
+
 """ Initialize the bot """
 bot = Bot(
 	command_prefix=when_mentioned_or('..', '>', '.'),
 	description="A list of commands available",
-	intents=intents
+	intents=intents,
+	debug_guilds=[GUILD]
 )
 
 @bot.event
@@ -17,20 +27,19 @@ async def on_ready():
 	print(f"Logged in as {bot.user} (ID: {bot.user.id})")
 	print("------")
 
-""" Load the bot token """
-from dotenv import load_dotenv
-load_dotenv()
-from os import getenv
-TOKEN = getenv("BOT_TOKEN")
 """ Load cogs """
 from os import listdir
-for file in listdir("cogs"):
-	if not file.endswith(".py"):
-		continue
-	name = file[:-3]
-	bot.load_extension(f"cogs.{name}")
-""" Run the bot """
-bot.run(TOKEN)
+def list_cogs():
+	cog_list = []
+	for file in listdir("cogs"):
+		if not file.endswith(".py"):
+			continue
+		cog_list.append(file[:-3])
+	return cog_list
+
+cogs = list_cogs()
+for cog in cogs:
+	bot.load_extension(f"cogs.{cog}")
 
 # TODO: web server for jukebox and/or soundboard?
 # TODO: cog to announce when someone joins vc (maybe delete messages after some time?)
@@ -39,3 +48,46 @@ bot.run(TOKEN)
 # TODO: warn or kick or ban a user?
 # TODO: filter the audit logs for a user? maybe?
 # TODO: keep stats or levels? ehhhh
+
+# ================================= ADMIN ======================================
+from discord.commands import Option, permissions
+
+ROLE_ADMIN = 518625964763119616
+ROLE_ADMIN_SFW = 727205354353721374
+ME = 201046736565829632
+
+async def cog_autocomplete(ctx: discord.AutocompleteContext):
+	return list_cogs()
+
+# @bot.command(name='reload')
+# @permissions.permission(role_id=ROLE_ADMIN, permission=True)
+# @permissions.permission(role_id=ROLE_ADMIN_SFW, permission=True)
+# @permissions.permission(user_id=ME, permission=True)
+# async def reload_prefix(ctx, cog: str = None):
+# 	"""Reload an extension (admin command)"""
+# 	if not cog:
+# 		return await ctx.send("Please specify a cog to reload")
+# 	bot.reload_extension(f"cogs.{cog}")
+# 	await ctx.send(f"Reloaded `{cog}` extension")
+
+@bot.slash_command(
+	name='reload',
+	guild_ids=[GUILD],
+	permissions=[
+		permissions.CommandPermission(id=ME, type=2, permission=True),
+		permissions.CommandPermission(id=ROLE_ADMIN, type=1, permission=True),
+		permissions.CommandPermission(id=ROLE_ADMIN_SFW, type=1, permission=True),
+	]
+)
+async def reload_slash(
+	ctx: discord.ApplicationContext,
+	cog: Option(str, "The cog to be reloaded", autocomplete=cog_autocomplete)
+):
+	"""Reload an extension (admin command)"""
+	bot.reload_extension(f"cogs.{cog}")
+	await ctx.respond(f"Reloaded `{cog}` extension", ephemeral=True)
+
+# ================================== END =======================================
+
+""" Run the bot """
+bot.run(TOKEN)
--- cogs/random.py
+++ cogs/random.py
@@ -6,21 +6,26 @@ from os import getenv
 from collections import Counter
 import re
 
+GUILD = getenv("GUILD_ID")
+
 def setup(bot):
 	bot.add_cog(Random(bot))
 
 class Random(Cog):
-	"""Use random.org to provide a truly random result"""
+	"""Use random.org to provide truly random results"""
 	def __init__(self, bot):
 		self.bot = bot
 		print("Initialized Random cog")
-	
+
+# ==============================================================================
+
 	def roll(self, *, faces: int = 6, number: int = 1):
+		"""Roll die(s) using random.org integer generation"""
 		columns = number if number < 11 else 10
 		return requests.get(f'https://www.random.org/integers/?num={number}&min=1&max={faces}&col={columns}&base=10&format=plain&rnd=new').text
 
 	def roll_results(self, *, faces: int = 6, number: int = 1):
-		"""Returns a formatted string to output"""
+		"""Returns a formatted result of dice outcomes"""
 		result = self.roll(faces=int(faces), number=int(number))
 		if faces == 100: # 00 - 99
 			result = re.sub('100', '00', result)
@@ -32,6 +37,7 @@ class Random(Cog):
 		aliases=['dice']
 	)
 	async def roll_prefix(self, ctx, query: str = '1d6'):
+		"""Roll some dice (default 1d6)"""
 		number, faces = query.split('d')
 		if not number: # handle raw dN rolls, e.g. 'd6' or 'd20'
 			number = 1
@@ -39,25 +45,33 @@ class Random(Cog):
 	
 	@slash_command(
 		name='roll',
-		guild_ids=[getenv("GUILD_ID")]
+		guild_ids=[GUILD]
 	)
 	async def roll_slash(self,
 		ctx: discord.ApplicationContext,
 		faces: Option(int, "How many faces are on each die?", min_value=1, max_value=1_000_000_000, default=6),
 		number: Option(int, "How many dies should be rolled?", min_value=1, max_value=10_000, default=1)
 	):
+		"""Roll some dice (default 1d6)"""
 		await ctx.respond(self.roll_results(faces=faces, number=number))
 
+# ==============================================================================
+
 	def flip(self, number: int = 1):
-		"""Flip a coin. Defaults to 1 flip."""
+		"""Returns a raw list of Heads or Tails outcomes"""
 		result = requests.get(f'https://www.random.org/integers/?num={number}&min=1&max=2&col=1&base=10&format=plain&rnd=new').text
 		result = result.replace('1', 'Heads')
 		result = result.replace('2', 'Tails')
 		return result
 	
 	def flip_results(self, number: int = 1):
-		"""Returns a formatted string to output"""
+		"""Returns a formatted result of Heads and Tails counts"""
 		result = self.flip(number)
+		if len(result) == 6: # \n counts as a character and we don't strip it
+			return (
+				f"*You flipped {number} coins and got:*\n"
+				f"**{result}**"
+			)
 		c = Counter(result.split('\n'))
 		return (
 			f"*You flipped {number} coins and got:*\n"
@@ -70,14 +84,16 @@ class Random(Cog):
 		aliases=['coin']
 	)
 	async def flip_prefix(self, ctx, number: str = 1):
+		"""Flip a coin (default 1)"""
 		await ctx.send(self.flip_results(number))
 
 	@slash_command(
 		name='flip',
-		guild_ids=[getenv("GUILD_ID")]
+		guild_ids=[GUILD]
 		)
 	async def flip_slash(self,
 		ctx: discord.ApplicationContext,
 		number: Option(int, "How many coins should be flipped?", min_value=1, max_value=10_000, default=1)
 	):
+		"""Flip a coin (default 1)"""
 		await ctx.respond(self.flip_results(number))
