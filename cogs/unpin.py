184a891c6d1eb002443e017f26db20762e830d28
a <a@trwnh.com>
2022-03-27 00:05:02 -0500

    add more logging to unpin cache

--- cogs/unpin.py
+++ cogs/unpin.py
@@ -31,12 +31,15 @@ class Unpin(Cog):
 		self.guild = await self.bot.fetch_guild(int(getenv("GUILD_ID")))
 		# load cache from pickle if it exists
 		if not path.exists('.cache/messages'):
+			logger.info("creating .cache/messages folder")
 			makedirs('.cache/messages')
 		filename = '.cache/messages/pins.pickle'
 		self.cache = set()
 		if path.getsize(filename) > 0:
 			with open(filename, 'r+b') as f:
 				self.cache = pickle.load(f)
+				logger.info("loaded pins.pickle file")
+				logger.debug(f"{self.cache=}")
 		else:
 			# get pinned message IDs
 			channels = await self.guild.fetch_channels()
@@ -49,6 +52,7 @@ class Unpin(Cog):
 				for message in pins:
 					if message.id not in self.cache:
 						self.cache.add(message.id)
+						logger.debug(f"{message.id} added to pin cache")
 			# commit cacheset to file storage
 			with open(filename, 'w+b') as f:
 				pickle.dump(self.cache, f)
@@ -65,12 +69,15 @@ class Unpin(Cog):
 			logger.info("ignoring partial update with no payload.data.pinned")
 			return
 		
+		logger.debug(f"{self.cache=}")
 		pinned_after = payload.data['pinned']
 		pinned_before = message_id in self.cache
 		if pinned_after and not pinned_before: # Pinned already generates a native message, so ignore pins
 			self.cache.add(message_id) # but we should still cache this pin
+			logger.debug(f"{message_id} added to pin cache")
 			return
 		if pinned_before and not pinned_after: # Generate a notice for unpinned messages
+			logger.info(f"unpin detected for message {url}")
 			channel = self.bot.get_channel(channel_id)
 			message = payload.cached_message or await channel.fetch_message(message_id)
 			msg = await channel.send(
